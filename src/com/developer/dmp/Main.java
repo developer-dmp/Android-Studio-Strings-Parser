0cef8ae77154973939ee6512f145462af14e9390
Domenic Polidoro <developer.dmp@gmail.com>
2018-03-22 21:06:35 -0400

    Initial commit

---
+++ src/com/developer/dmp/Main.java
@@ -0,0 +1,17 @@
+package com.developer.dmp;
+
+import java.io.File;
+
+public class Main {
+
+    private static final String XML_PART_1 = "<string name = \"";
+    private static final String XML_PART_2 = "\">";
+    private static final String XML_PART_3 = "</string>";
+
+    public static void main(String[] args) {
+
+        File fileStringName = new File("input/strings_input.txt");
+
+
+    }
+}
